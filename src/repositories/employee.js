102f928c4f6510d65b279ac38fdbe80398e70fb7
Asaromi <a.sabililromi@gmail.com>
2024-01-25 16:52:33 +0700

    fix: remove useless repo method

--- src/repositories/employee.js
+++ src/repositories/employee.js
@@ -6,13 +6,6 @@ class EmployeeRepository extends BaseRepository {
     super(Employee)
     this.model = Employee
   }
-
-  async saveEmployeeModel(employee) {
-    if (!employee || !(employee instanceof Employee)) throw new Error('employee required and must be an instance of Employee')
-
-    console.log('employee updated')
-    return await this.saveModel(employee)
-  }
 }
 
 module.exports = EmployeeRepository
