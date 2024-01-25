f0932ca6e72ce7d82bd34894ad6d30b1271d1eb2
Asaromi <a.sabililromi@gmail.com>
2024-01-25 16:53:23 +0700

    fix: add deleteEmployee method

--- src/services/employee.js
+++ src/services/employee.js
@@ -1,18 +1,40 @@
 const generateId = require('../libs/ulid')
+const AttendanceService = require('./attendance')
 const EmployeeRepository = require('../repositories/employee')
+const { BadRequestError } = require('../libs/exceptions')
+const { debug } = require('../libs/response')
 
 class EmployeeService {
   constructor() {
     this.employeeRepository = new EmployeeRepository()
+    this.attendanceService = new AttendanceService()
+  }
+
+  async deleteEmployeeModel(employee) {
+    const deleted = await this.attendanceService.deleteAttendanceByEmployeeId(employee.id) || {}
+    if (!deleted) throw new BadRequestError('Failed to delete attendance or attendance with employeeId not found')
+
+    deleted.employee = await this.employeeRepository.deleteModel(employee)
+    if (!deleted.employee) throw new BadRequestError('Failed to delete employee')
+    debug('employee deleted!', deleted.employee)
+
+    return deleted
+  }
+
+  async getEmployeeByUserId(userId, options = {}) {
+    if (!userId) throw new BadRequestError('userId is required')
+
+    const query = { userId }
+    return await this.employeeRepository.getBy({ query, options })
   }
 
   async saveEmployeeModel(employee) {
-    return await this.employeeRepository.saveEmployeeModel(employee)
+    return await this.employeeRepository.saveModel(employee)
   }
 
   generateEmployeeModel(data) {
     if (data?.constructor !== Object) throw new Error('data must be an object')
-    else if (!data.userId) throw new Error('userId is required')
+    else if (!data.userId) throw new BadRequestError('userId is required')
 
     if (!data.id) data.id = generateId()
 
